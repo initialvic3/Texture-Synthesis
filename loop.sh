504eabb926a9a168714795ea63427f55b903bb94
victor chan <initialvic3@gmail.com>
2020-11-13 00:02:00 +1100

    changes

--- loop.sh
+++ loop.sh
@@ -1,4 +1,6 @@
 for num in $(seq 2 40)
 do
-	python texture_synthesis.py --image_path floor/$num.jpg --block_size 60 --num_block 8 --mode Cut
+	python texture_synthesis.py --out_dir results --image_path floor/$num.jpg --block_size 60 --num_block 8 --mode Cut
 done
+
+
--- notes.md
+++ notes.md
@@ -1,3 +1,6 @@
 conda activate Texture-Synthesis
 conda install pip
 pip install numpy scikit-image PIL argparse pygt5
+
+Based on this paper here:
+https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/papers/efros-siggraph01.pdf
